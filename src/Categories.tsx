7268f25972ce2893a656d30a40f1d128f198622a
ArtyomZakharenko <artiomzaharenko@gmail.com>
2022-09-13 18:50:18 +0300

    updated Readme

---
+++ src/Categories.tsx
@@ -0,0 +1,20 @@
+function Categories({ categories, filterItems }: any) {
+	return (
+		<div className="btn-container">
+			{categories.map((category: string, index: number) => {
+				return (
+					<button
+						type="button"
+						className="filter-btn"
+						key={index}
+						onClick={() => filterItems(category)}
+					>
+						{category}
+					</button>
+				);
+			})}
+		</div>
+	);
+};
+
+export default Categories;
